b91ad32a4d4d40e530c51d82bb212c3b6cb7585c
baronet2 <eth.baron@mail.utoronto.ca>
2022-07-07 23:58:09 -0400

    Package structure

---
+++ R/shotmissr-package.R
@@ -0,0 +1,16 @@
+#' The 'shotmissr' package.
+#'
+#' @description A DESCRIPTION OF THE PACKAGE
+#'
+#' @docType package
+#' @name shotmissr-package
+#' @aliases shotmissr
+#' @useDynLib shotmissr, .registration = TRUE
+#' @import methods
+#' @import Rcpp
+#' @importFrom rstan sampling
+#'
+#' @references
+#' Stan Development Team (2020). RStan: the R interface to Stan. R package version 2.21.2. https://mc-stan.org
+#'
+NULL
