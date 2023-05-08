b5a1663b01af1aa8b05241bad12789c100394b89
baronet2 <eth.baron@mail.utoronto.ca>
2023-05-07 22:40:30 -0400

    Script to fit mixture model weights

---
+++ data-raw/mixture_model_weights.R
@@ -0,0 +1,50 @@
+devtools::load_all()
+
+shots_data <- statsbomb_shots_processed |>
+  prepare_shooting_skill_data()
+
+mixture_model_components <- get_mixture_model_components()
+
+pdfs <- get_shot_probability_densities(
+  mixture_model_components,
+  shots = shots_data |>
+    dplyr::select(y_end_proj, z_end_proj) |>
+    as.matrix()
+)
+
+global_weights <- fit_global_weights(pdfs, iter = 500, seed = 42)
+selected_components <- which(global_weights > 0.01)
+usethis::use_data(selected_components, overwrite = TRUE)
+
+half_season_shots_data <- statsbomb_shots_processed |>
+  prepare_shooting_skill_data() |>
+  dplyr::group_by(player, Season) |>
+  dplyr::mutate(first_half_season = dplyr::row_number() < dplyr::n() / 2) |>
+  dplyr::ungroup()
+
+shooting_skill_data <- get_player_groups(
+  half_season_shots_data,
+  grouping_cols = c("player", "Season", "first_half_season"),
+  group_size_threshold = 5
+)
+
+usethis::use_data(shooting_skill_data, overwrite = TRUE)
+
+pdfs <- get_shot_probability_densities(
+  mixture_model_components[selected_components,],
+  shooting_skill_data |> dplyr::select(y_end_proj, z_end_proj) |> as.matrix()
+)
+
+mixture_model_fit <- fit_player_weights(
+  pdfs,
+  shooting_skill_data$group_id,
+  alpha = 30,
+  iter = 500,
+  seed = 42
+)
+
+global_weights <- mixture_model_fit[["global_weights"]]
+usethis::use_data(global_weights, overwrite = TRUE)
+
+player_weights <- mixture_model_fit[["player_weights"]]
+usethis::use_data(player_weights, overwrite = TRUE)
